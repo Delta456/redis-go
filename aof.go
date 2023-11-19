fecdc2e06b648c440262a4cf77574f550fc462f5
Swastik <swstkbaranwal@gmail.com>
2023-11-20 01:01:44 +0530

    add aof

---
+++ aof.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"os"
+	"sync"
+	"time"
+)
+
+// AOF (Append only file): In this method, Redis records each command in the file as RESP.
+// When a restart occurs, Redis reads all the RESP commands from the AOF file and executes them in memory.
+
+// RDB (Redis Database): This is a snapshot of the data that is created at regular intervals according to the configuration.
+// For example, every 3 minutes or every 10 minutes, depending on how you configure it.
+// In this method, Redis takes a complete copy of the data in memory and saves it to a file.
+// When a restart or crash occurs, the data is reloaded from the RDB file.
+
+type Aof struct {
+	file *os.File
+	rd   *bufio.Reader
+	mu   sync.Mutex
+}
+
+func NewAof(path string) (*Aof, error) {
+	f, err := os.OpenFile(path, os.O_CREATE|os.O_RDWR, 0666)
+	if err != nil {
+		return nil, err
+	}
+
+	aof := &Aof{
+		file: f,
+		rd:   bufio.NewReader(f),
+	}
+
+	// Start a goroutine to sync AOF to disk every 1 second
+	go func() {
+		for {
+			aof.mu.Lock()
+
+			aof.file.Sync()
+
+			aof.mu.Unlock()
+
+			time.Sleep(time.Second)
+		}
+	}()
+
+	return aof, nil
+}
+
+func (aof *Aof) Close() error {
+	aof.mu.Lock()
+	defer aof.mu.Unlock()
+
+	return aof.file.Close()
+}
+
+func (aof *Aof) Write(value Value) error {
+	aof.mu.Lock()
+	defer aof.mu.Unlock()
+
+	_, err := aof.file.Write(value.Marshal())
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (aof *Aof) Read(fn func(value Value)) error {
+	aof.mu.Lock()
+	defer aof.mu.Unlock()
+
+	aof.file.Seek(0, io.SeekStart)
+
+	reader := NewResp(aof.file)
+
+	for {
+		value, err := reader.Read()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+
+			return err
+		}
+
+		fn(value)
+	}
+
+	return nil
+}
